d6a9ced7c32de307fbaa9f468af8e83180b3bb08
Florent FREMONT <ff.fremont.florent@gmail.com>
2023-01-14 12:01:31 +0100

    fix: lazy loading img gallery

--- site/themes/atlanticvet-theme/assets/js/app.js
+++ site/themes/atlanticvet-theme/assets/js/app.js
@@ -1,5 +1,8 @@
-$(document).ready(function() {
-    $("ul.gallery").lightGallery({
-        getCaptionFromTitleOrAlt:false
-    }); 
+$(document).ready(function () {
+  $("ul.gallery").lightGallery({
+    getCaptionFromTitleOrAlt: false,
+    thumbnail:false
+  });
+  const observer = lozad(); // lazy loads elements with default selector as '.lozad'
+  observer.observe();
 });
